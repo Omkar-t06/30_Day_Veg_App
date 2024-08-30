9b09ab58d252f92bf01e15a6f13f539f75594c0a
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-08-31 00:10:43 +0530

    Added App Bar

--- app/src/main/java/com/omkar/a30daysofveg/MainActivity.kt
+++ app/src/main/java/com/omkar/a30daysofveg/MainActivity.kt
@@ -4,18 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.omkar.a30daysofveg.data.DishesList
-import com.omkar.a30daysofveg.model.DishModel
 import com.omkar.a30daysofveg.ui.theme._30DaysOfVegTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,8 +21,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             _30DaysOfVegTheme {
-
+                A30DaysOfVeg()
             }
         }
+    }
+}
+
+@Composable
+fun A30DaysOfVeg() {
+    Scaffold(topBar = { MyAppBar() }) { innerPadding ->
+        RecipeListView(
+            dishesList = DishesList.loadDishes,
+            contentPadding = innerPadding,
+        )
+    }
+}
+
+@Composable
+fun MyAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+        },
+        modifier = modifier
+    )
+}
+
+
+@Preview
+@Composable
+fun AppPreview() {
+    _30DaysOfVegTheme {
+        A30DaysOfVeg()
     }
 }
--- app/src/main/java/com/omkar/a30daysofveg/RecipeListView.kt
+++ app/src/main/java/com/omkar/a30daysofveg/RecipeListView.kt
@@ -1,2 +1,42 @@
 package com.omkar.a30daysofveg
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.omkar.a30daysofveg.data.DishesList
+import com.omkar.a30daysofveg.model.DishModel
+
+@Composable
+fun RecipeListView(
+    modifier: Modifier = Modifier,
+    dishesList: List<DishModel>,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+) {
+    LazyColumn(
+        contentPadding = contentPadding,
+        modifier = modifier
+    ) {
+        itemsIndexed(dishesList) { index, dish ->
+            RecipeCard(
+                dayNumber = index + 1,
+                recipeOfDish = dish,
+                modifier = modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_small),
+                    vertical = dimensionResource(id = R.dimen.padding_extra_small)
+                )
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListPreview() {
+    RecipeListView(dishesList = DishesList.loadDishes)
+}
